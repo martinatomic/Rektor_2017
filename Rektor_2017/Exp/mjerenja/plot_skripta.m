fad142772c1fcd9b03719b4d59245394541e53b7
Matija Bedeković <matija.bedekovic@gmail.com>
2017-04-25 21:34:28 +0200

    Modified log ploting script, added plots
    Also added zip file containing logs of rotor experiment

--- Rektor_2017/Exp/mjerenja/plot_skripta.m
+++ Rektor_2017/Exp/mjerenja/plot_skripta.m
@@ -45,14 +45,48 @@ legend('Referenca','Mjerenje');
 xlabel('t [s]');
 ylabel('\omega [rad/s]');
 
+%% eksperiment s rotorima
+%unzip
+unzip('mjerenja_s_rotorima.zip');
 
+%ucitaj podatke iz fajla
+rot_ang_mv = csvread('bag_roll_mv.txt',1,0);
+rot_ang_ref = csvread('bag_roll_reff.txt',1,0);
+rot_rate_mv = csvread('bag_roll_rate_mv.txt',1,0);
+rot_rate_ref = csvread('bag_roll_rate_reff.txt',1,0);
 
+%napravi offset vremena na 0 i skaliranje na sekunde 
+rot_ang_mv(:,1) = rot_ang_mv(:,1)-ones(length(rot_ang_mv(:,1)),1)*rot_ang_mv(1,1);
+rot_ang_mv(:,1) = rot_ang_mv(:,1)/1e9;
 
+rot_ang_ref(:,1) = rot_ang_ref(:,1)-ones(length(rot_ang_ref(:,1)),1)*rot_ang_ref(1,1);
+rot_ang_ref(:,1) = rot_ang_ref(:,1)/1e9;
 
+rot_rate_mv(:,1) = rot_rate_mv(:,1)-ones(length(rot_rate_mv(:,1)),1)*rot_rate_mv(1,1);
+rot_rate_mv(:,1) = rot_rate_mv(:,1)/1e9;
 
+rot_rate_ref(:,1) = rot_rate_ref(:,1)-ones(length(rot_rate_ref(:,1)),1)*rot_rate_ref(1,1);
+rot_rate_ref(:,1) = rot_rate_ref(:,1)/1e9;
 
+%sad plotaj
+figure
+plot(rot_ang_ref(:,1),rot_ang_ref(:,2));
+hold on; grid on;
+plot(rot_ang_mv(:,1),rot_ang_mv(:,2));
+axis tight 
+legend('Referenca','Mjerenje');
+xlabel('t [s]');
+ylabel('\theta [rad]');
 
+figure
+plot(rot_rate_ref(:,1),rot_rate_ref(:,2));
+hold on; grid on;
+plot(rot_rate_mv(:,1),rot_rate_mv(:,2));
+axis tight 
+legend('Referenca','Mjerenje');
+xlabel('t [s]');
+ylabel('\omega [rad/s]');
 
-
+delete 'bag_roll_mv.txt' 'bag_roll_reff.txt' 'bag_roll_rate_mv.txt' 'bag_roll_rate_reff.txt'
 
 
